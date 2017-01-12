9a4744e2bf8376e6f8304c29badfac88b976a3d5
tchl <whtchl@126.com>
2017-01-12 11:11:51 +0800

    init project

---
+++ app/src/main/java/zhj/viewgroupdemo/ButtonItem.java
@@ -0,0 +1,18 @@
+package zhj.viewgroupdemo;
+
+
+public class ButtonItem {
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    int position;
+    public ButtonItem(int position) {
+        this.position=position;
+    }
+
+}
---
+++ app/src/main/java/zhj/viewgroupdemo/CreditCard.java
@@ -0,0 +1,41 @@
+package zhj.viewgroupdemo;
+
+
+public class CreditCard {
+
+
+    public String getEdittext1() {
+        return edittext1;
+    }
+
+    public void setEdittext1(String edittext1) {
+        this.edittext1 = edittext1;
+    }
+
+    public String getEdittext2() {
+        return edittext2;
+    }
+
+    public void setEdittext2(String edittext2) {
+        this.edittext2 = edittext2;
+    }
+
+    private String edittext1;
+    private String edittext2;
+    private int type;
+    CreditCard(String edittext1,String edittext2,int type){
+        this.edittext1=edittext1;
+        this.edittext2=edittext2;
+        this.type=type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+
+}
